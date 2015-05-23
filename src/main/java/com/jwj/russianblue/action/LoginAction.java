309ee4e20230a2b593f6bd7de12a11a4c3638adb
jingwei027git <jingwei027@gmail.com>
2015-05-23 23:09:24 +0800

    test

---
+++ src/main/java/com/jwj/russianblue/action/LoginAction.java
@@ -0,0 +1,13 @@
+package com.jwj.russianblue.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class LoginAction extends ActionSupport {
+
+	/** ¤¤¤å¦r **/
+	public String execute() {
+		return SUCCESS;
+	}
+
+}

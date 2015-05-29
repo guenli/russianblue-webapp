67d5687d079722ebe046877d85fcf3987e4987c6
jingwei027git <jingwei027@gmail.com>
2015-05-30 00:38:37 +0800

    ddd

---
+++ src/main/java/com/jwj/russianblue/core/service/RbService.java
@@ -0,0 +1,9 @@
+package com.jwj.russianblue.core.service;
+
+import java.io.Serializable;
+
+import com.jwj.russianblue.core.entity.RbModel;
+
+public interface RbService<T extends RbModel> extends Serializable {
+
+}
---
+++ src/main/java/com/jwj/russianblue/core/service/RbServiceImpl.java
@@ -0,0 +1,8 @@
+package com.jwj.russianblue.core.service;
+
+import com.jwj.russianblue.core.entity.RbModel;
+
+@SuppressWarnings("serial")
+public abstract class RbServiceImpl<T extends RbModel> implements RbService<T> {
+
+}
